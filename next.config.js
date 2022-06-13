a58b0ad4dc9c12c2517bdb31b49a5b69f703f116
vietnguyen97 <53213535+vietnguyen97@users.noreply.github.com>
2022-06-13 09:50:06 +0700

    update fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpack: (config, {}) => {
-        config.module.rules.push({
-            test: /\.scss$/,
-            loader: ["style-loader", "css-loader", "sass-loader"],
-        });
-        return config;
-    },
 };
 
 module.exports = nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,29 @@ import Document, {
     NextScript,
     DocumentContext,
 } from "next/document";
+import fs from "fs";
+import path from "path";
+
+class CustomNextHead extends Head {
+    // TODO: This might not be needed if Next.js implements built-in support
+    // https://github.com/zeit/next-plugins/issues/364
+    getCssLinks({ allFiles }: any) {
+        return allFiles
+            .filter((file: any) => file.endsWith(".css"))
+            .map((file: any) => (
+                <style
+                    key={file}
+                    nonce={this.props.nonce}
+                    dangerouslySetInnerHTML={{
+                        __html: fs.readFileSync(
+                            path.join(".next", file),
+                            "utf-8"
+                        ),
+                    }}
+                />
+            ));
+    }
+}
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
@@ -28,6 +51,7 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
+                <CustomNextHead />
                 <Head />
                 <body>
                     <Main />
