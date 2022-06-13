e97faa07720b11e7c2a8659c61b9c4a62834e09f
vietnguyen97 <53213535+vietnguyen97@users.noreply.github.com>
2022-06-13 12:06:27 +0700

    BNS fix prod #2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,41 +1,25 @@
-import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentContext,
-} from "next/document";
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
 
-        // Run the React rendering logic synchronously
-        ctx.renderPage = () =>
-            originalRenderPage({
-                // Useful for wrapping the whole react tree
-                enhanceApp: (App: any) => App,
-                // Useful for wrapping in a per-page basis
-                enhanceComponent: (Component: any) => Component,
-            });
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: App => props =>
+                        sheet.collectStyles(<App {...props} />),
+                });
 
-        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-        const initialProps = await Document.getInitialProps(ctx);
-
-        return initialProps;
-    }
-
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: [initialProps.styles, sheet.getStyleElement()],
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 }
-
-export default MyDocument;
